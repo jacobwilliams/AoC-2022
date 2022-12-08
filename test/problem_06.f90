c662c06d5f8229f51a14a1c0d1bf52d2755ca25d
Jacob Williams <jacobwilliams@users.noreply.github.com>
2022-12-07 19:03:01 -0600

    day 6 cleanup

--- test/problem_06.f90
+++ test/problem_06.f90
@@ -5,38 +5,32 @@ program problem_6
 
     implicit none
 
-    integer :: i, j, k, n, iunit
+    integer :: i, iunit
     character(len=:),allocatable :: line
 
     open(newunit=iunit, file='inputs/day6.txt', status='OLD')
     line = read_line(iunit)
     close(iunit)
+
+    i = go(4);  write(*,*) '6a: number characters processed', i
+    i = go(14); write(*,*) '6b: number characters processed', i
+
+    contains
+
+    function go(step) result(i)
+    integer,intent(in) :: step
+    integer :: i
+    integer :: j, k, n
     n = len(line)
-
-    main : do i = 4, n
-        do j = i-3, i
-            do k = i-3, i
+    main : do i = step, n
+        do j = i-(step-1), i
+            do k = i-(step-1), i
                 if (j==k) cycle ! skip this
-                if (line(j:j)==line(k:k)) then
-                    cycle main ! not it
-                end if
+                if (line(j:j)==line(k:k)) cycle main ! not it
             end do
         end do
-        exit
+        exit ! found it
     end do main
-    write(*,*) '6a: number characters processed', i
-
-    main2 : do i = 14, n
-        do j = i-13, i
-            do k = i-13, i
-                if (j==k) cycle ! skip this
-                if (line(j:j)==line(k:k)) then
-                    cycle main2 ! not it
-                end if
-            end do
-        end do
-        exit
-    end do main2
-    write(*,*) '6b: number characters processed', i
+    end function go
 
 end program problem_6
